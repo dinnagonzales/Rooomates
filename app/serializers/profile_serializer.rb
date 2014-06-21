08fd5ba6b6e000a5ddd9b7d94fe4b768c0dd8e4a
dinnagonzales <dinnagonzales.05@gmail.com>
2014-06-21 16:21:28 -0700

    Home skeleton 60% complete

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,3 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :cleanliness, :noise_tolerance, :visitors, :pets, :smoking, :sleeping_hours, :cooks, :lifestyle, :interests, :notes, :user_id
+  attributes :id, :cleanliness, :noise_tolerance, :visitors, :pets, :smoking, :sleeping_hours, :cooks, :lifestyle, :interests, :notes, :user_id, :user
 end
