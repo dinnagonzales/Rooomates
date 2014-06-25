eea04e99daddde75475ddcec8f8cf242e2a79469
dinnagonzales <dinnagonzales.05@gmail.com>
2014-06-25 14:59:09 -0700

    USERS HAVE PICTURESgit add -Agit add -A

--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -1,3 +1,4 @@
 class ProfileSerializer < ActiveModel::Serializer
-  attributes :id, :cleanliness, :noise_tolerance, :visitors, :pets, :smoking, :sleeping_hours, :cooks, :lifestyle, :interests, :notes, :user_id, :user
+  attributes :id, :cleanliness, :noise_tolerance, :visitors, :pets, :smoking, :sleeping_hours, :cooks, :lifestyle, :interests, :notes, :user_id
+  has_one :user, serializer: UserShortSerializer
 end
---
+++ app/serializers/user_short_serializer.rb
@@ -0,0 +1,7 @@
+class UserShortSerializer < ActiveModel::Serializer
+  attributes :id, :avatar_url, :name, :age, :city
+  def avatar_url
+  	object.avatar.url(:square)
+  end
+
+end
